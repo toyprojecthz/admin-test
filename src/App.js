59b9fc960e11efe1c77af75a5766db72d93659b7
Hyeunjin <kliketoy@gmail.com>
2022-08-01 23:30:04 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  let [title, setTitle] = useState(["강가가", "가강강", "난나"]);
+  let [modal, setModal] = useState(false);
+  function updateTitle() {
+    let copy = [...title];
+    copy[0] = "바뀐텍스트";
+    setTitle(copy);
+  }
+
+  function sortList() {
+    const copy = [...title];
+    var result = copy.sort();
+    setTitle(result);
+    console.log(result);
+  }
+  function Modal() {
+    return (
+      <div>
+        <h4>제목</h4>
+        <p>내용</p>
+        <span>날짜</span>
+      </div>
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => sortList()}>가나다정렬</button>
+      <button onClick={() => updateTitle()}>텍스트변경</button>
+      <ul>
+        {title.map((item) => {
+          return (
+            <li
+              onClick={() => {
+                modal === true ? setModal(false) : setModal(true);
+              }}
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+
+      <p>2.17</p>
+      {modal === true ? <Modal /> : null}
     </div>
   );
 }
